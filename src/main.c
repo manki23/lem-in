a86ce4d9e8c954d91000046158f69d991011b306
Myriam ANKI <manki@student.42.fr>
2019-09-03 19:24:40 +0200

    almost empty main.c added

---
+++ src/main.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manki <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/03 19:09:18 by manki             #+#    #+#             */
+/*   Updated: 2019/09/03 19:23:24 by manki            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/lem_in.h"
+/*
+static void		ft_check_line(char *in)
+{
+	fuction to check if a line is correct
+}
+
+
+static void		ft_stock_input(void)
+{
+	fuction to stock the input in our structure
+}
+*/
+int		main(void)
+{
+	/*
+	char	*input;
+
+	while (get_next_line(0, &input) == 1)
+	{
+		if (!ft_check_line(input))
+			ft_error("ERROR", 2);
+		ft_stock_input();
+		ft_strdel(&input);
+	}
+	*/
+	return (0);
+}
