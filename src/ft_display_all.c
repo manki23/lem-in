c5f8daa605a7665940465f7d808cdf895a278b32
Yohann DANA <yodana@e1r8p1.42.fr>
2019-09-29 18:58:10 +0200

    Norme faite

--- src/ft_display_all.c
+++ src/ft_display_all.c
@@ -6,96 +6,84 @@
 /*   By: manki <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 14:35:59 by manki             #+#    #+#             */
-/*   Updated: 2019/09/26 11:28:12 by manki            ###   ########.fr       */
+/*   Updated: 2019/09/29 18:56:10 by yodana           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/lem_in.h"
 
-void	print_sol(t_room **tmp, int i)
+void	print_sol(t_room **tmp, int *i, int c_ants)
 {
-	if (tmp[i])
-	{			
-		ft_printf("L%d-%s", i + 1, tmp[i]->name);
-		tmp[i] = tmp[i]->child;
+	if (tmp[*i])
+	{
+		ft_printf("L%d-%s", *i + 1, tmp[*i]->name);
+		tmp[*i] = tmp[*i]->child;
 	}
+	if (*i < c_ants)
+		*i = *i + 1;
 }
 
-void	first_display(int *ants, t_queue *display, t_room **tmp, int c_ants)
+int		first_display_check(int o, int size, t_room **tmp, t_queue *d)
+{
+	if (o < size && tmp[0] && d->print == 1)
+		ft_printf(" ");
+	tmp[0] ? d->print = 1 : d->print;
+	return (1);
+}
+
+void	first_display(int *ants, t_queue *d, t_room **tmp, int c_ants)
 {
 	int size;
 	int o;
 	int i;
 	int j;
-	int print;
 
-	print = 0;
-	size = ft_queue_len(display);
-	while (total_ants(ants, ft_queue_len(display)) > 0)
+	size = ft_queue_len(d);
+	while (total_ants(ants, ft_queue_len(d)) > 0)
 	{
 		i = 0;
-		o = 0;
+		o = -1;
 		j = 0;
-		while (o < size)
+		while (++o < size)
 		{
-			if (ants[j] > 0)
+			if (ants[j] > 0 && first_display_check(o, size, &tmp[i], d) == 1)
 			{
-				if (o < size && tmp[i] && print == 1)
-					ft_printf(" ");
-				if (tmp[i])
-					print = 1;
-				print_sol(tmp, i);
-				i < c_ants - 1 ? i++ : i;
-				o >= size - ft_queue_len(display) ? ants[j]-- : ants[j];
+				print_sol(tmp, &i, c_ants);
+				o >= size - ft_queue_len(d) ? ants[j]-- : ants[j];
 			}
-			if (o >= size - ft_queue_len(display) && j < ft_queue_len(display) - 1)
+			if (o >= size - ft_queue_len(d) && j < ft_queue_len(d) - 1)
 				j++;
-			o++;
 		}
-		print = 0;
+		d->print = 0;
 		ft_printf("\n");
-		size = size + ft_queue_len(display);
+		size = size + ft_queue_len(d);
 	}
 }
 
-int	sol_one_queue(t_queue *sol)
+void	display_sol(t_queue **display, int c_ants, int i, int print)
 {
-	if (ft_queue_len(sol) == 1)
-	{
-		if (calc_nodes(sol->room) == 1)
-			return (1);
-	}
-	return (0);
-}
+	t_room	**tmp;
+	int		*ants;
 
-void	display_sol(t_queue **display, int c_ants)
-{
-	int i;
-	t_room **tmp;
-	int *ants;
-	int print;
-
-	print = 0;
-	i = 0;
-	tmp = (t_room **)malloc(sizeof(t_room *) * (c_ants + 1));
-	ants = malloc(sizeof(int*) * ft_queue_len(display[0]));
+	if (!(tmp = (t_room **)malloc(sizeof(t_room *) * (c_ants + 1))))
+		return ;
+	if (!(ants = malloc(sizeof(int*) * ft_queue_len(display[0]))))
+		return ;
 	chose_ants(ants, c_ants, ft_queue_len(display[0]), &display[0]);
 	stock_room_sol(tmp, display[0], c_ants);
 	first_display(ants, display[0], tmp, c_ants);
 	while (check_end(tmp, c_ants) == -1)
 	{
-		while (i < c_ants)
+		while (++i < c_ants)
 		{
 			if (tmp[i] && print == 1)
 				ft_printf(" ");
-			if (tmp[i])
-				print = 1;
-			print_sol(tmp, i);
-			i++;
+			tmp[i] ? print = 1 : print;
+			print_sol(tmp, &i, i - 1);
 		}
 		print = 0;
 		ft_printf("\n");
-		i = 0;
+		i = -1;
 	}
 	free(tmp);
 	free(ants);
@@ -106,11 +94,12 @@ void	display(t_queue **sol, int ants)
 	int i;
 
 	i = 1;
-	if (sol_one_queue(sol[0]) == 1)
+	sol[0]->print = 0;
+	if (ft_queue_len(*sol) == 1 && calc_nodes((*sol)->room) == 1)
 	{
 		while (i <= ants)
 		{
-			ft_printf("L%d-%s", i + 1, sol[0]->room->name);
+			ft_printf("L%d-%s", i, sol[0]->room->name);
 			if (i < ants)
 				ft_printf(" ");
 			i++;
@@ -118,5 +107,5 @@ void	display(t_queue **sol, int ants)
 		ft_printf("\n");
 	}
 	else
-		display_sol(sol, ants);
+		display_sol(sol, ants, -1, 0);
 }
